e5fea6a64c1c9f3ad256b15aa5b34aa9ee4314ad
Mohamed El Amine DADDOU <mea.daddou@gmail.com>
2021-05-03 15:19:38 +0100

    Backend: remove unused code

--- packages/backend/src/resolvers/resolvers.ts
+++
@@ -1,5 +0,0 @@
-import UserResolver from "Modules/users/User.resolver";
-import AuthorResolver from "Modules/authors/Author.resolver";
-import QuoteResolver from "Modules/quotes/Quote.resolver";
-
-export const resolvers = [UserResolver, AuthorResolver, QuoteResolver] as const;
