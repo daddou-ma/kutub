5040d0736070faf4aae40f246339c7109d480b9d
Mohamed El Amine DADDOU <mea.daddou@gmail.com>
2021-05-09 03:43:30 +0100

    Separating Routes Components

--- packages/frontend/src/components/App.tsx
+++ packages/frontend/src/components/App.tsx
@@ -4,24 +4,15 @@ import { setContext } from "@apollo/client/link/context";
 import { createUploadLink } from "apollo-upload-client";
 import { relayStylePagination } from "@apollo/client/utilities";
 import { persistCache, LocalStorageWrapper } from "apollo3-cache-persist";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { BrowserRouter as Router } from "react-router-dom";
 import { create } from "jss";
 import rtl from "jss-rtl";
 import { StylesProvider, jssPreset } from "@material-ui/core/styles";
 import { ThemeProvider } from "Hooks/useTheme";
 
-import { QuotePage } from "Pages/Quotes";
-import { LibraryPage } from "Pages/Library";
-import { FavoriteQuotePage } from "Pages/FavoriteQuotes";
-import { ReaderPage } from "Pages/Reader";
-import { LoginPage } from "Pages/Login";
-import { SettingsPage } from "Pages/Settings";
-import { LanguageSettingsPage } from "Pages/LanguageSettings";
-import { CustomRoute, Role } from "Components/CustomRoute";
 import { AuthProvider } from "Hooks/useAuth";
 import { SnackbarProvider } from "Hooks/useSnackbar";
-import ReactGA from "react-ga";
+import { Routes } from "Components/Routes";
 
 const GRAPH_URL = process.env.GRAPHQL_URL;
 
@@ -105,13 +96,9 @@ const authLink = setContext((_, { headers }) => {
 
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 
-const history = createBrowserHistory();
-history.listen((location) => {
-  ReactGA.pageview(location.pathname + location.search);
-});
-
 export function App(): React.ReactElement {
   const [client, setClient] = useState(null);
+
   useEffect(() => {
     async function init() {
       const cache = new InMemoryCache({
@@ -161,57 +148,8 @@ export function App(): React.ReactElement {
         <StylesProvider jss={jss}>
           <ThemeProvider>
             <SnackbarProvider>
-              <Router history={history}>
-                <Switch>
-                  <CustomRoute
-                    path="/library"
-                    role={Role.USER}
-                    redirect="login"
-                  >
-                    <LibraryPage />
-                  </CustomRoute>
-                  <CustomRoute path="/login" role={Role.GUEST} redirect="/">
-                    <LoginPage />
-                  </CustomRoute>
-                  <CustomRoute
-                    path="/quotes"
-                    role={Role.USER}
-                    redirect="/login"
-                  >
-                    <QuotePage />
-                  </CustomRoute>
-                  <CustomRoute
-                    path="/favorites"
-                    role={Role.USER}
-                    redirect="/login"
-                  >
-                    <FavoriteQuotePage />
-                  </CustomRoute>
-                  <CustomRoute
-                    path="/reader/:epubId"
-                    role={Role.USER}
-                    redirect="/login"
-                  >
-                    <ReaderPage />
-                  </CustomRoute>
-                  <CustomRoute
-                    path="/settings/language"
-                    role={Role.USER}
-                    redirect="/login"
-                  >
-                    <LanguageSettingsPage />
-                  </CustomRoute>
-                  <CustomRoute
-                    path="/settings"
-                    role={Role.USER}
-                    redirect="/login"
-                  >
-                    <SettingsPage />
-                  </CustomRoute>
-                  <CustomRoute path="/" role={Role.USER} redirect="/login">
-                    <LibraryPage />
-                  </CustomRoute>
-                </Switch>
+              <Router>
+                <Routes />
               </Router>
             </SnackbarProvider>
           </ThemeProvider>
---
+++ packages/frontend/src/components/Routes.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { Switch, useHistory } from "react-router-dom";
+
+import { QuotePage } from "Pages/Quotes";
+import { LibraryPage } from "Pages/Library";
+import { FavoriteQuotePage } from "Pages/FavoriteQuotes";
+import { ReaderPage } from "Pages/Reader";
+import { LoginPage } from "Pages/Login";
+import { SettingsPage } from "Pages/Settings";
+import { LanguageSettingsPage } from "Pages/LanguageSettings";
+import { CustomRoute, Role } from "Components/CustomRoute";
+import ReactGA from "react-ga";
+
+export function Routes(): React.ReactElement {
+  const history = useHistory();
+
+  useEffect(
+    () =>
+      history.listen((location) => {
+        console.log(location.pathname + location.search);
+        ReactGA.pageview(location.pathname + location.search);
+      }),
+    []
+  );
+
+  return (
+    <Switch>
+      <CustomRoute path="/library" role={Role.USER} redirect="login">
+        <LibraryPage />
+      </CustomRoute>
+      <CustomRoute path="/login" role={Role.GUEST} redirect="/">
+        <LoginPage />
+      </CustomRoute>
+      <CustomRoute path="/quotes" role={Role.USER} redirect="/login">
+        <QuotePage />
+      </CustomRoute>
+      <CustomRoute path="/favorites" role={Role.USER} redirect="/login">
+        <FavoriteQuotePage />
+      </CustomRoute>
+      <CustomRoute path="/reader/:epubId" role={Role.USER} redirect="/login">
+        <ReaderPage />
+      </CustomRoute>
+      <CustomRoute path="/settings/language" role={Role.USER} redirect="/login">
+        <LanguageSettingsPage />
+      </CustomRoute>
+      <CustomRoute path="/settings" role={Role.USER} redirect="/login">
+        <SettingsPage />
+      </CustomRoute>
+      <CustomRoute path="/" role={Role.USER} redirect="/login">
+        <LibraryPage />
+      </CustomRoute>
+    </Switch>
+  );
+}
